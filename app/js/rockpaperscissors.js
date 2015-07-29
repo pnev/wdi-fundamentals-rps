cfeaa85c21cf082eaa41b0db0aead9afe3c807fc
pnev <pamela.nevins@gmail.com>
2015-07-29 19:39:07 -0400

    Provided code for body of getWinner(playerMove, computerMove) function

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -40,8 +40,20 @@ function getWinner(playerMove,computerMove) {
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
-    return winner;
+    if ((playerMove==='paper' && computerMove==='rock')||
+    (playerMove==='rock' && computerMove==='scissors')||
+    (playerMove==='scissors' && computerMove==='paper'))
+    winner = 'player';
+
+    else if ((playerMove==='scissors' && computerMove==='rock')||
+    (playerMove==='rock' && computerMove==='paper')||
+    (playerMove==='paper' && computerMove==='scissors'))
+    winner = 'computer';
+
+    else
+    winner = 'tie';  
+
+return winner;
 }
 
 function playToFive() {
