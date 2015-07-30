2acea034d7873c443152cd9f7253f5569bf9800d
pnev <pamela.nevins@gmail.com>
2015-07-30 11:05:19 -0400

    Provided code for body of playToFive() function

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -61,7 +61,17 @@ function playToFive() {
     var playerWins = 0;
     var computerWins = 0;
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
+    while (playerWins < 5 && computerWins < 5) {
+        var playerMove = getPlayerMove();
+        var computerMove = getComputerMove();
+        var winner = getWinner(playerMove, computerMove);
+            if(winner === 'player')
+            playerWins++;
+            else if(winner === 'computer')
+            computerWins++;
+        console.log("Player chose " + playerMove + " while Computer chose " + computerMove);
+        console.log("The score is currently " + playerWins + " to " + computerWins + '\n');
+    }    
     return [playerWins, computerWins];
 }
 
